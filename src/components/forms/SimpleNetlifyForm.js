c117b9b20605c0c7eac72be7b38d873c00d8e577
arhoy <arhoy@ualberta.ca>
2019-10-05 23:42:47 -0600

    added form and identity

--- src/components/forms/SimpleNetlifyForm.js
+++ src/components/forms/SimpleNetlifyForm.js
@@ -1,7 +1,48 @@
 import React from 'react';
 
 const SimpleNetlifyForm = () => {
-  return <div>This is a simple netlify form</div>;
+  return (
+    <form
+      name="contact"
+      method="POST"
+      data-netlify="true"
+      netlify-honeypot="bot-field"
+      action="/thank-you"
+    >
+      <p class="hidden">
+        <label>
+          Don’t fill this out if you're human: <input name="bot-field" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Your Name: <input type="text" name="name" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Your Email: <input type="email" name="email" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Your Role:{' '}
+          <select name="role[]" multiple>
+            <option value="leader">Leader</option>
+            <option value="follower">Follower</option>
+          </select>
+        </label>
+      </p>
+      <p>
+        <label>
+          Message: <textarea name="message"></textarea>
+        </label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
+  );
 };
 
 export default SimpleNetlifyForm;
