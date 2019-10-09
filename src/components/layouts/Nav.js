01ac29801716dbb1a00ffed19a992b279ab85716
arhoy <arhoy@ualberta.ca>
2019-10-09 01:11:54 -0600

    dropdown menu achieved

--- src/components/layouts/Nav.js
+++ src/components/layouts/Nav.js
@@ -1,44 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'gatsby';
+
 import styled from '@emotion/styled';
 import netlifyIdentity from 'netlify-identity-widget';
 import { FaBeer, FaAlignRight } from 'react-icons/fa';
 import links from '../../constants/navLinks';
-
-const Navlink = styled(Link)`
-  color: ${props => props.theme.colors.primary};
-  font-size: 1.8rem;
-  text-decoration: none;
-  margin-right: 1rem;
-
-  &:hover {
-    color: ${props => props.theme.colors.primaryDark};
-    font-weight: bolder;
-  }
-  &.currentPage {
-    color: ${props => props.theme.colors.primaryDark};
-  }
-`;
-
-const NetlifyID = styled.span`
-  .netlify-identity-user,
-  .netlify-identity-button,
-  .netlify-identity-menu {
-    color: ${props => props.theme.colors.primary};
-    font-size: 1.8rem;
-    text-decoration: none;
-    margin-right: 1rem;
-    text-decoration: none !important;
-
-    &:hover {
-      color: ${props => props.theme.colors.primaryDark};
-      font-weight: bolder;
-    }
-    &.currentPage {
-      color: ${props => props.theme.colors.primaryDark};
-    }
-  }
-`;
+import DropDownMenu1 from '../Links/DropDownMenu1';
+import Navlink from '../Links/NavLink';
 
 const Header = styled.header`
   background: ${props => props.theme.colors.lightgrey};
@@ -112,7 +79,8 @@ const Nav = () => {
             {link.text}
           </Navlink>
         ))}
-        <NetlifyID data-netlify-identity-button></NetlifyID>
+        <DropDownMenu1 />
+
         <a
           href="https://ko-fi.com/alexquasar"
           target="_blank"
